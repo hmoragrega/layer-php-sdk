f23419acca8525a8516e8d8ec57ef401a468a05f
Hilari Moragrega <hilari.moragrega@teamcmp.com>
2016-05-23 15:29:50 +0200

    Improved tests scripts

--- spec/ClientBuilderSpec.php
+++ spec/ClientBuilderSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\UglyGremlin\Layer;
 
+use PhpSpec\Exception\Example\SkippingException;
 use PhpSpec\ObjectBehavior;
 use UglyGremlin\Layer\Http\ClientInterface;
 use UglyGremlin\Layer\Uuid\UuidGeneratorInterface;
@@ -30,5 +31,27 @@ class ClientBuilderSpec extends ObjectBehavior
         $this->withUuidGenerator($uuidGenerator);
 
         $this->build()->shouldReturnAnInstanceOf('UglyGremlin\Layer\Client');
-    } 
+    }
+
+    function it_can_build_the_client_with_ramsey_uuid_generator(ClientInterface $httpClient)
+    {
+        if (!class_exists('Ramsey\Uuid\Uuid') && !class_exists('Rhumsaa\Uuid\Uuid')) {
+            throw new SkippingException('The require dependency is not installed');
+        }
+
+        $this->withHttpClient($httpClient);
+
+        $this->build()->shouldReturnAnInstanceOf('UglyGremlin\Layer\Client');
+    }
+
+    function it_can_build_the_client_with_guzzle(UuidGeneratorInterface $uuidGenerator)
+    {
+        if (!class_exists('Guzzle\Http\Client') && !class_exists('GuzzleHttp\Client')) {
+            throw new SkippingException('The require dependency is not installed');
+        }
+
+        $this->withUuidGenerator($uuidGenerator);
+
+        $this->build()->shouldReturnAnInstanceOf('UglyGremlin\Layer\Client');
+    }
 }
